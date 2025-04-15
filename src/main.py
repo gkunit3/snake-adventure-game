c5bc2cf742503f9a1f18c30ca80b61759bcb1f59
gkunit3 <gkunit3@protonmail.com>
2025-04-15 09:25:58 +0530

    Initial game loop with Pygame

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+    clock.tick(60)
